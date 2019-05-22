643532bccb0a4dccb8cf6d6914f4f7e2a39ff035
Shanti Chellaram <shanti@shanti.im>
2019-05-22 14:55:54 -0400

    try out comrak

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use std::io::{Read, Result, stdin};
 use pulldown_cmark::{Parser, Event, Tag};
+use comrak::{parse_document, Arena, ComrakOptions};
 
 /* Prints the first (h1) title of a markdown document */
 
@@ -21,16 +22,16 @@ fn main() -> Result<()> {
     let mut markdown_input: String = String::from("");
     stdin().lock().read_to_string(&mut markdown_input)?;
 
-    let mut parser = Parser::new(&markdown_input);
     match mode {
         RunMode::Title => {
+            let mut parser = Parser::new(&markdown_input);
             let title = parse_title(&mut parser).unwrap_or(String::from(""));
             println!("{}", title);
         },
         RunMode::Dump => {
-            for event in parser {
-                dbg!(event);
-            }
+            let arena = Arena::new();
+            let foo = comrak::parse_document(&arena, &markdown_input, &ComrakOptions::default());
+            dbg!(foo);
         }
     }
     Ok(())
